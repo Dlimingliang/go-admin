be9eb0f2f43f5a711e6b2d6a5d9c3cffbe4aea82
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-06-10 15:54:46 +0800

    zap加入

--- main.go
+++ main.go
@@ -1,30 +1,12 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/Dlimingliang/go-admin/global"
 	"github.com/Dlimingliang/go-admin/initialize"
 )
 
 func main() {
 	initialize.InitViper()
 	initialize.InitZap()
-	global.GaLog.Info("zap 打印Info日志测试")
-	global.GaLog.Warn("zap 打印Warn日志测试")
-	global.GaLog.Error("zap 打印Error日志测试")
 	//初始化数据库
 	initialize.InitGorm()
-	testGorm()
-}
-
-type Test struct {
-	Id   int
-	Name string
-}
-
-func testGorm() {
-	var test Test
-	global.GaDb.Find(&test, 1)
-	fmt.Println(test.Id, test.Name)
 }
