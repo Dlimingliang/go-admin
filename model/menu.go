4551e04791da0317a4acd10df69a32ba87daab68
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-06-29 22:14:01 +0800

    角色加入

--- model/menu.go
+++ model/menu.go
@@ -9,15 +9,15 @@ type Menu struct {
 	Component     string        `json:"component" gorm:"type:varchar(40);not null;comment:前端文件路径"`              //前端文件路径
 	Sort          int           `json:"sort" gorm:"not null;comment:排序"`                                        //排序
 	ParentId      int           `json:"parentId" gorm:"column:parent_id;not null;default:0;comment:父菜单ID"`      //父菜单id
-	Children      []Menu        `json:"children" gorm:"-"`
-	Parameters    []interface{} `json:"parameters" gorm:"-"`
-	MenuBtn       []interface{} `json:"menuBtn" gorm:"-"`
-	SysAuthoritys []interface{} `json:"authoritys" gorm:"-"`
+	Children      []Menu        `json:"children" gorm:"-"`                                                      //子菜单
+	SysAuthoritys []interface{} `json:"authoritys" gorm:"-"`                                                    //关联角色
+	Parameters    []interface{} `json:"parameters" gorm:"-"`                                                    //开发中
+	MenuBtn       []interface{} `json:"menuBtn" gorm:"-"`                                                       //开发中
 }
 
 type Meta struct {
 	Name      string `json:"title" gorm:"type:varchar(20);not null;unique;comment:菜单名称"` //菜单名称
 	Icon      string `json:"icon" gorm:"type:varchar(20);not null;comment:图标"`           //图标
-	KeepAlive bool   `json:"keepAlive" gorm:"-"`                                         // 是否缓存
-	CloseTab  bool   `json:"closeTab" gorm:"-"`                                          // 自动关闭tab
+	KeepAlive bool   `json:"keepAlive" gorm:"-"`                                         // 开发中
+	CloseTab  bool   `json:"closeTab" gorm:"-"`                                          // 开发中
 }
