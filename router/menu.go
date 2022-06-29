a34d2679a4502ec1e082c392b783097431f1b043
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-06-29 22:19:12 +0800

    角色加入

--- router/menu.go
+++ router/menu.go
@@ -9,14 +9,14 @@ import (
 func InitMenuRouter(group *gin.RouterGroup) {
 	menuGroup := group.Group("menu")
 	{
-		menuGroup.POST("addBaseMenu", v1.CreateMenu)
-		menuGroup.POST("updateBaseMenu", v1.UpdateMenu)
-		menuGroup.POST("deleteBaseMenu", v1.DeleteMenu)
+		menuGroup.POST("addBaseMenu", v1.CreateMenu)    //新建菜单
+		menuGroup.POST("updateBaseMenu", v1.UpdateMenu) //修改菜单
+		menuGroup.POST("deleteBaseMenu", v1.DeleteMenu) //删除菜单
 	}
 	menuGroupWithoutRecord := group.Group("menu")
 	{
-		menuGroupWithoutRecord.POST("getBaseMenuTree", v1.GetMenuTree)
-		menuGroupWithoutRecord.POST("getMenuList", v1.GetMenuList)
-		menuGroupWithoutRecord.POST("getBaseMenuById", v1.GetMenuById)
+		menuGroupWithoutRecord.POST("getMenuList", v1.GetMenuList)     //分页获取菜单列表
+		menuGroupWithoutRecord.POST("getBaseMenuTree", v1.GetMenuTree) //获取菜单树
+		menuGroupWithoutRecord.POST("getBaseMenuById", v1.GetMenuById) //根据id获取菜单信息
 	}
 }
