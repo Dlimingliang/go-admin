d0318a7fa1592c2087f226b3cdb0afdd89f1cbf1
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-07-22 10:43:22 +0800

    优雅地终止服务

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"go.uber.org/zap"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Dlimingliang/go-admin/global"
 	"github.com/Dlimingliang/go-admin/initialize"
@@ -23,9 +31,30 @@ func main() {
 	initialize.InitValidator()
 	//migrate()
 
-	if err := ginRouter.Run(fmt.Sprintf(":%d", global.ServerConfig.SystemConfig.ServerPort)); err != nil {
-		global.GaLog.Panic(err.Error())
+	//启动服务
+	address := fmt.Sprintf(":%d", global.ServerConfig.SystemConfig.ServerPort)
+	srv := &http.Server{
+		Addr:    address,
+		Handler: ginRouter,
 	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+			global.GaLog.Error(err.Error())
+		}
+	}()
+	time.Sleep(10 * time.Microsecond)
+	global.GaLog.Info("go-admin启动成功", zap.String("address", address))
+
+	//优雅地终止服务
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		global.GaLog.Error("go-admin退出失败!", zap.Error(err))
+	}
+	global.GaLog.Info("go-admin退出成功")
 }
 
 func migrate() {
